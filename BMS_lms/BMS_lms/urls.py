46141c96ca632d5892ddc2d443a44a3f25daa451
abhuvan345 <112195084+Bhuvan345@users.noreply.github.com>
2024-10-24 22:41:01 +0530

    4th video half done home,contactus, course page done

--- BMS_lms/BMS_lms/urls.py
+++ BMS_lms/BMS_lms/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('base', views.BASE, name='base'),
+
+    path('',views.HOME,name='home'),
+    path('single/course',views.SINGLE_COURSE,name='single_course'),
+    path('contact',views.CONTACT_US,name='contact_us'),
 ]
--- BMS_lms/BMS_lms/views.py
+++ BMS_lms/BMS_lms/views.py
@@ -4,3 +4,15 @@ from django.template.context_processors import request
 
 def BASE(request):
     return render(request,'base.html')
+
+
+def HOME(request):
+    return render(request,'Main/home.html')
+
+
+def SINGLE_COURSE(request):
+    return render(request,'Main/single_course.html')
+
+
+def CONTACT_US(request):
+    return render(request,'Main/contact_us.html')
