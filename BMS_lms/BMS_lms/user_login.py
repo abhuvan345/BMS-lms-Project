6fca4074265537f7283aae4c5070c3ebf3726446
abhuvan345 <112195084+Bhuvan345@users.noreply.github.com>
2024-10-25 22:44:30 +0530

    was creating registration backend

--- BMS_lms/BMS_lms/user_login.py
+++ BMS_lms/BMS_lms/user_login.py
@@ -2,4 +2,10 @@ from django.shortcuts import render,redirect
 
 
 def REGISTER(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+
     return render(request,'registration/register.html')
