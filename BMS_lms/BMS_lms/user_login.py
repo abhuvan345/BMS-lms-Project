bfbff2a3ce4c9c7db4c1c336d6568085cdeb14fc
abhuvan345 <112195084+Bhuvan345@users.noreply.github.com>
2024-10-26 22:46:57 +0530

    10:32 in video 5 of registration urls and msg.html page

--- BMS_lms/BMS_lms/user_login.py
+++ BMS_lms/BMS_lms/user_login.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
+from pyexpat.errors import messages
 
 
 def REGISTER(request):
@@ -7,5 +9,22 @@ def REGISTER(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
+        # check email
+        if User.objects.filter(email=email).exists():
+            messages.warning(request, 'Email Already Exists !')
+            return redirect('register')
+
+        #check username
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, 'Username Already exists !')
+            return redirect('register')
+
+        user =User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('login')
 
     return render(request,'registration/register.html')
