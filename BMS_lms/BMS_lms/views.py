16bdc59f635541aefd36b8de7bf8c75d872b4b3c
abhuvan345 <112195084+Bhuvan345@users.noreply.github.com>
2024-11-10 12:54:40 +0530

    filter by category and level using ajaxQuery done sucessfully!!!

--- BMS_lms/BMS_lms/views.py
+++ BMS_lms/BMS_lms/views.py
@@ -1,9 +1,11 @@
 from lib2to3.fixes.fix_input import context
 
+from django.http import JsonResponse
 from django.shortcuts import redirect,render
 from django.template.context_processors import request
 from app.models import Categories,Course,Level
 from unicodedata import category
+from django.template.loader import render_to_string
 
 
 
@@ -25,11 +27,31 @@ def HOME(request):
 def SINGLE_COURSE(request):
     category = Categories.get_all_category(Categories)
     level = Level.objects.all()
+    course = Course.objects.all()
     context= {
         'category' : category,
         'level' : level,
+        'course' : course,
     }
     return render(request,'Main/single_course.html',context)
+
+def filter_data(request):
+    category = request.GET.getlist('category[]')
+    level = request.GET.getlist('level[]')
+
+
+    if category:
+        course = Course.objects.filter(category__id__in = category).order_by('-id')
+    elif level:
+        course = Course.objects.filter(level__id__in = level).order_by('-id')
+    else:
+        course = Course.objects.all().order_by('-id')
+
+    context = {
+        'course' : course
+    }
+    t = render_to_string('ajax/course.html', context)
+    return JsonResponse({'data' : t})
 
 
 def CONTACT_US(request):
@@ -38,3 +60,5 @@ def CONTACT_US(request):
 
 def ABOUT_US(request):
     return render(request,'Main/about_us.html')
+
+
