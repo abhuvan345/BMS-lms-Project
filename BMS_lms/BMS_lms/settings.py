9934ed958137f9b25e4fd2c3d1a13df45abf0c8b
abhuvan345 <112195084+Bhuvan345@users.noreply.github.com>
2024-12-18 23:49:47 +0530

    change the allowed host to all i.e *

--- BMS_lms/BMS_lms/settings.py
+++ BMS_lms/BMS_lms/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5=@2ix!kwl=8v_9d@2&ifdyh%c2(c+3+q^_vakgy1%%$-ac+--
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
